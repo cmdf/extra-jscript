ae551dcfafe84ba36ce22563bf6e6407d953faf6
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 06:20:08 +0530

    program: get jscript.net compiler path

--- cs-jscript/Program.cs
+++ cs-jscript/Program.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.IO;
 
 namespace orez.jscript {
 	class Program {
 		static void Main(string[] args) {
+			var jsc = GetJscPath();
+			var jtmp = evar("temp") + "\\0rez\\cs-jscript";
+
+		}
+
+		// get jscript.net compiler
+		private static string GetJscPath() {
+			// prepare
+			var jpth = evar("systemroot") + "\\microsoft.net\\framework\\";
+			var jptn = "*jsc.exe";
+			// search jscript.net compiler
+			var jsc = Environment.GetEnvironmentVariable("JSC_PATH");
+			if(jsc == null) {
+				foreach(var a in Directory.GetFiles(jpth, jptn, SearchOption.AllDirectories))
+					jsc = a;
+				evar("JSC_PATH", jsc);
+			}
+			return jsc;
+		}
+
+		// shortcut to get environment variable
+		private static string evar(string name) {
+			return Environment.GetEnvironmentVariable(name);
+		}
+
+		// shortcut to set environment variable
+		private static void evar(string name, string value) {
+			Environment.SetEnvironmentVariable(name, value);
 		}
 	}
 }
