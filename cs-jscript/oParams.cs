23c12dcaad3fcd21cbed7c26e2d1ce3ee18aa815
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 15:52:46 +0530

    oparams: a class for the input options

---
+++ cs-jscript/oParams.cs
@@ -0,0 +1,22 @@
+﻿namespace orez.jscript {
+	class oParams {
+
+		/// <summary>
+		/// Optional. Tells whether the file is only to be compiled, not run.
+		/// In such case, full path of the output binary should be output.
+		/// </summary>
+		public bool compile = false;
+		/// <summary>
+		/// Optional. Indicates the output path of the compiled script.
+		/// </summary>
+		public string output = null;
+		/// <summary>
+		/// Indicates the input script to be compiled.
+		/// </summary>
+		public string input;
+		/// <summary>
+		/// Input arguments to the script.
+		/// </summary>
+		public string args;
+	}
+}
