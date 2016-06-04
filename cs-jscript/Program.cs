18f767810ba7a7cd8901254e967b736937bc7219
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 05:56:55 +0530

    program: namespace updated and unnecessary usings removed

--- cs-jscript/Program.cs
+++ cs-jscript/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace cs_jscript {
+namespace orez.jscript {
 	class Program {
 		static void Main(string[] args) {
 		}
