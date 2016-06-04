354d49bfa4493acac330d6a6477bd56a27abf91a
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 15:59:46 +0530

    program: add boring comments and keep loc high

--- cs-jscript/Program.cs
+++ cs-jscript/Program.cs
@@ -5,27 +5,23 @@ namespace orez.jscript {
 	class Program {
 		static void Main(string[] args) {
 			// prepare
-			var jpth = evar("systemroot")+"\\microsoft.net\\framework\\";
-			var jtmp = evar("temp") + "\\0rez\\cs-jscript";
-			var jptn = "*jsc.exe";
-			// search jscript.net compiler
-			var JSC_PATH = Environment.GetEnvironmentVariable("JSC_PATH");
-			if(JSC_PATH == null) {
-				foreach(var a in Directory.GetFiles(jpth, jptn, SearchOption.AllDirectories))
-					JSC_PATH = a;
-				evar("JSC_PATH", JSC_PATH);
-			}
 
 		}
 
+		// get jscript.net compiler
 		private static string GetJscPath() {
-			var JSC_PATH = Environment.GetEnvironmentVariable("JSC_PATH");
-			if(JSC_PATH == null) {
+			// prepare
+			var jpth = evar("systemroot") + "\\microsoft.net\\framework\\";
+			var jtmp = evar("temp") + "\\0rez\\cs-jscript";
+			var jptn = "*jsc.exe";
+			// search jscript.net compiler
+			var jsc = Environment.GetEnvironmentVariable("JSC_PATH");
+			if(jsc == null) {
 				foreach(var a in Directory.GetFiles(jpth, jptn, SearchOption.AllDirectories))
-					JSC_PATH = a;
-				evar("JSC_PATH", JSC_PATH);
+					jsc = a;
+				evar("JSC_PATH", jsc);
 			}
-			return JSC_PATH;
+			return jsc;
 		}
 
 		// shortcut to get environment variable
