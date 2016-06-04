780e78456f4238d3058a8b6d00eaf0a2126d1b63
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 15:55:31 +0530

    program: created shortcut method to get environment variable value

--- cs-jscript/Program.cs
+++ cs-jscript/Program.cs
@@ -6,11 +6,17 @@ namespace orez.jscript {
 		static void Main(string[] args) {
 			// prepare
 			var jpth = Environment.GetEnvironmentVariable("systemroot")+"\\microsoft.net\\framework\\";
+			var jtmp = 
 			// search jscript.net compiler
 			var JSC_PATH = Environment.GetEnvironmentVariable("JSC_PATH");
 			if(JSC_PATH == null) {
 				foreach(var a in Directory.GetFiles()
 			}
+		}
+
+		// shortcut for get environment variable
+		private static string evar(string name) {
+			return Environment.GetEnvironmentVariable(name);
 		}
 	}
 }
